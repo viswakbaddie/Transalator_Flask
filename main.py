1be24b324fc5817e6a048d70c7493f31e26ee93b
viswakggautham <viswakggautham@gmail.com>
2023-09-17 20:15:51 +0300

    commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from transformers import pipeline
+res = '<transalated text>'
+app = Flask(__name__)
+pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-fi")
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    global res
+    if request.method == 'POST':
+        name= request.form.get('english')
+        res = pipe(name)
+        res = res[0]['translation_text']
+        print(name)
+    return render_template("index.html", res=res)
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
